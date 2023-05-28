17f23c5697fb799c9becc2fa87f1e675cf5f52d1
Ceylan <ceylanbtml41@gmail.com>
2023-05-28 21:07:40 +0300

    v1

--- src/test/java/techproed/day02_DriverMethods/C04_ManageMethods_ImplicitlyWait.java
+++ src/test/java/techproed/day02_DriverMethods/C04_ManageMethods_ImplicitlyWait.java
@@ -11,6 +11,7 @@ public class C04_ManageMethods_ImplicitlyWait {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
